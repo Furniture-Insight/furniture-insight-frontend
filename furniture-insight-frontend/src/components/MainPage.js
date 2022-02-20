01f84856bfba081a78aa1588f3fb66f05afa8ebf
Sebastian Soto <sebastiansoto.fms@gmail.com>
2022-02-20 16:42:23 -0400

    Created Main Page branch and component

---
+++ furniture-insight-frontend/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainPage() {
+    return(
+        <div>
+            <div>
+                <h3>Furniture Insight</h3>
+            </div>           
+        </div>        
+    )
+}
+export default MainPage;
--- furniture-insight-frontend/src/index.js
+++ furniture-insight-frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from "./components/LoginPage";
+import MainPage from "./components/MainPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LoginPage/>
+    <MainPage/>
   </React.StrictMode>,
   document.getElementById('root')
 );
