86f176d5f820faabce7b290a4eed915e4ff0b922
Sebastian Soto <sebastiansoto.fms@gmail.com>
2022-02-28 15:37:33 -0400

    Navbar progress

--- furniture-insight-frontend/src/components/Navbar.js
+++ furniture-insight-frontend/src/components/Navbar.js
@@ -1,23 +1,39 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
 
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+      if(window.innerWidth <= 960) {
+          setButton(false);
+      } else {
+          setButton(true);
+      }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
-                    Furniture Insight
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                    Furniture Insight <i className='fab fa-typo3' />
                 </Link>
-                <div className='menu-icon'>
-                    <i className={click ? 'fas fa-times' : 'fas-bars'} />
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fas-bars'} />
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
@@ -40,7 +56,13 @@ function Navbar() {
                             About Us
                         </Link>
                     </li>
+                    <li>
+                        <Link to='/SignUp' className='nav-links' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
     </>
