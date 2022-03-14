a8ee14a891d11e0d4afed717ffe91934e0692cc3
Sebastian Soto <sebastiansoto.fms@gmail.com>
2022-03-13 21:30:26 -0400

    Edited Home and Description pages

--- furniture-insight-frontend/src/components/pages/Description.js
+++ furniture-insight-frontend/src/components/pages/Description.js
@@ -1,11 +1,28 @@
 import React from "react";
+import bed from "../images/bed.jpg";
 
 function Description() {
     return (
-        <div className="description">
-            <div className="headerContainer">
-                <h3> Furniture Name </h3>
-                <h5> Description </h5>
+        <div className="container">
+            <div className="row">
+                <div className="col">   
+                    <img className="center img-thumbnail" src={bed} alt="bed.jpg"/>
+                </div>
+                <div className="col">
+                    <h3 className="h3 text-center"> Nice Bed </h3>
+                    <p className="h6 text-center"> This is the bed description </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p className="h6"> Price: </p>
+                </div>
+                <div className="col">
+                    <button
+                    type="button"
+                    className="btn btn-success rounded-pill w-25"
+                    data-bs-toggle> Add to Cart </button>
+                </div>
             </div>
         </div>
     );
--- furniture-insight-frontend/src/components/pages/Home.js
+++ furniture-insight-frontend/src/components/pages/Home.js
@@ -1,13 +1,38 @@
 import React from "react";
-import BannerImage from "../images/bed.jpg"
+import bed from "../images/bed.jpg";
 
 function Home() {
     return (
-        <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-            <div className="headerContainer">
-                <h1> Furniture Insight </h1>
-            </div>           
-        </div>  
+        <div className="container">
+            <div className="row mt-3">
+                <div className="col mb-3">
+                    <h2 className="h2 text-center"> Furniture Insight </h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">   
+                    <img className="center img-thumbnail" src={bed} alt="bed.jpg"/>
+                </div>
+                <div className="col">
+                    <h3 className="h3 text-center"> Nice Bed </h3>
+                    <p className="h6 text-center"> This is the bed description </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button
+                    type="button"
+                    className="btn btn-success rounded-pill w-25"
+                    data-bs-toggle> See more like this </button>
+                </div>
+                <div className="col">
+                    <button
+                    type="button"
+                    className="btn btn-success rounded-pill w-25"
+                    data-bs-toggle> Details </button>
+                </div>
+            </div>
+        </div>
     );
 }
 
