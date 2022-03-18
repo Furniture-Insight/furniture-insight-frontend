75eb408d73fa5832a2bc91f320783259bd592246
Jose Mendoza <joseamendozajimenez@hotmail.com>
2022-03-18 13:51:12 -0500

    Created Navbar file and moved nav to the file

--- furniture-insight-frontend/src/App.js
+++ furniture-insight-frontend/src/App.js
@@ -1,54 +1,33 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import logo from "./components/images/logo.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStore, faRightToBracket, faUserPlus, faCartShopping, faHouse, faTableCells } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./components/pages/Home"
+import Login from "./components/pages/Login";
+import SignUp from "./components/pages/SignUp";
+import RecoverPass from "./components/pages/RecoverPass";
+import Store from "./components/pages/Store";
+import Description from "./components/pages/Description";
+import SmartRoom from './components/pages/SmartRoom';
+import Cart from "./components/pages/Cart";
+import Checkout from "./components/pages/Checkout";
+import Navbar from './components/Navbar';
 
 function App() {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-end">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/store">
-                        <img src={logo} width="40" height="40" alt="logo" className="me-2" />
-                        Furniture Insight
-                    </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse navbarToggler" >
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 nav nav-tabs">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/Home"><FontAwesomeIcon icon={faHouse} /> Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/store"><FontAwesomeIcon icon={faStore} /> Store</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/cart"><FontAwesomeIcon icon={faCartShopping} /> Cart</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/smartRoom"><FontAwesomeIcon icon={faTableCells} /> Smart Room</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/description">Description</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="collapse navbar-collapse navbarToggler">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 nav nav-tabs">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login"><FontAwesomeIcon icon={faRightToBracket} /> Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/signup"><FontAwesomeIcon icon={faUserPlus} /> SignUp</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <Outlet />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navbar/>}>
+                    <Route path="home" element={<Home />}></Route>
+                    <Route path="login" element={<Login />}></Route>
+                    <Route path="signup" element={<SignUp />}></Route>
+                    <Route path="recoverpass" element={<RecoverPass />}></Route>
+                    <Route path="store" element={<Store />}></Route>
+                    <Route path="description" element={<Description />}></Route>
+                    <Route path="smartRoom" element={<SmartRoom />}></Route>
+                    <Route path="cart" element={<Cart />}></Route>
+                    <Route path="checkout" element={<Checkout />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
