a901865002cd47015a90945886653a0f7fb5b8ea
Jose Mendoza <joseamendozajimenez@hotmail.com>
2022-03-27 22:15:36 -0500

    Users and Clients can be created from Sign Up

--- furniture-insight-frontend/src/components/pages/Home.js
+++ furniture-insight-frontend/src/components/pages/Home.js
@@ -5,7 +5,7 @@ import chairs from "../images/chairs.jpg";
 function Home() {
     return (
         <div align="center">
-            <div id="carouselHomeSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+            <div id="carouselHomeSlidesOnly" className="carousel slide " data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <img src={bed} className="d-block w-25" alt="bed.jpg"/>
@@ -17,8 +17,13 @@ function Home() {
                         <img src={chairs} className="d-block w-25" alt="chairs.jpg"/>
                     </div>
                 </div>
-                <button className="carousel-control-prev" type="">
-
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselHomeSlidesOnly" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>                    
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselHomeSlidesOnly" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>                    
                 </button>
             </div>
         </div>
--- furniture-insight-frontend/src/components/pages/SignUp.js
+++ furniture-insight-frontend/src/components/pages/SignUp.js
@@ -1,34 +1,55 @@
 import React from "react";
-import {useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../images/logo.png";
 
-function SignUp({isUserLogged}) {
+function SignUp({ isUserLogged }) {
     let navigate = useNavigate();
 
     const [usuario, setUsuario] = useState({
         Id_Genero: "",
         Nombre: "",
         Apellido: "",
-        Contraseña: "",        
+        Contraseña: "",
         Edad: "",
         Direccion_Residencia: ""
     });
-   
-    const handleSubmit = (event) => {        
-        event.preventDefault();
-        fetch('http://localhost:8000/user/createUser', {
+
+    const [cliente, setCliente] = useState({
+        Id_Usuario: "",
+        Email: ""
+    })
+
+
+    const createUsuario = () => {        
+        fetch('http://localhost:8000/user/crear', {
             method: 'POST',
-            headers: {"Content-Type" : "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(usuario)
-        }).then(() =>{
-            console.log(usuario);
-            console.log("new user created");
-        })
+        }).then((response) => response.json())
+        .then(data => this.setCliente({...cliente,Id_Usuario: data.Id_Usuario}))
+        .then(console.log(cliente))     
+    }
+
+    const createCliente = () => {        
+        fetch('http://localhost:8000/client/createClient', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(cliente)
+        }).then(()=> {
+            console.log(cliente)
+        })        
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        createUsuario();
+        createCliente();
     }
 
     const handleClick = () => {
-        isUserLogged(true);        
+        isUserLogged(true);
+        // navigate("/home", {replace:true});      
     }
 
     return (
@@ -37,62 +58,62 @@ function SignUp({isUserLogged}) {
                 <div className="form-login">
                     <img className="mb-3" src={logo} width="60" height="65" alt="logo" />
                     <h4 className="h4 mb-3">Sign Up</h4>
-                    <input 
+                    <input
                         required
-                        className="mb-3 form-control rounded-pill border border-dark" 
-                        type="text" 
-                        placeholder="Nombre" 
+                        className="mb-3 form-control rounded-pill border border-dark"
+                        type="text"
+                        placeholder="Nombre"
                         value={usuario.Nombre}
-                        onChange={(e) => setUsuario({...usuario, Nombre:e.target.value})}/>
-                    <input 
+                        onChange={(e) => setUsuario({ ...usuario, Nombre: e.target.value })} />
+                    <input
                         required
-                        className="mb-3 form-control rounded-pill border border-dark" 
-                        type="text" 
-                        placeholder="Apellido" 
+                        className="mb-3 form-control rounded-pill border border-dark"
+                        type="text"
+                        placeholder="Apellido"
                         value={usuario.Apellido}
-                        onChange={(e) => setUsuario({...usuario, Apellido:e.target.value})}/>
-                    {/* <input 
+                        onChange={(e) => setUsuario({ ...usuario, Apellido: e.target.value })} />
+                    <input
                         required
-                        className="mb-3 form-control rounded-pill border border-dark" 
-                        type="email" 
-                        placeholder="Email" 
-                        value={usuario.Email}
-                        onChange={(e) => setUsuario({...usuario, Email:e.target.value})}/> */}
-                    <input 
+                        className="mb-3 form-control rounded-pill border border-dark"
+                        type="email"
+                        placeholder="Email"
+                        value={cliente.Email}
+                        onChange={(e) => setCliente({ ...cliente, Email: e.target.value })} />
+                    <input
                         required
-                        className="mb-3 form-control rounded-pill border border-dark" 
-                        type="password" 
-                        placeholder="Password" 
+                        className="mb-3 form-control rounded-pill border border-dark"
+                        type="password"
+                        placeholder="Password"
                         value={usuario.Contraseña}
-                        onChange={(e) => setUsuario({...usuario, Contraseña:e.target.value})}/>
+                        onChange={(e) => setUsuario({ ...usuario, Contraseña: e.target.value })} />
                     <select
                         required
-                        className="mb-3 form-select rounded-pill border border-dark" 
-                        value={usuario.Id_Genero}  
-                        onChange={(e) => setUsuario({...usuario, Id_Genero:e.target.value})}>
+                        className="mb-3 form-select rounded-pill border border-dark"
+                        value={usuario.Id_Genero}
+                        onChange={(e) => setUsuario({ ...usuario, Id_Genero: e.target.value })}>
                         <option>Genero</option>
                         <option value="1">Hombre</option>
                         <option value="2">Mujer</option>
-                    </select>                   
-                    <input 
+                    </select>
+                    <input
                         required
-                        className="mb-3 form-control rounded-pill border border-dark" 
-                        type="number" 
+                        className="mb-3 form-control rounded-pill border border-dark"
+                        type="number"
                         placeholder="Edad"
                         value={usuario.Edad}
-                        onChange={(e) => setUsuario({...usuario, Edad:e.target.value})}/>
-                    <input 
+                        onChange={(e) => setUsuario({ ...usuario, Edad: e.target.value })} />
+                    <input
                         required
-                        className="mb-3 form-control rounded-pill border border-dark" 
-                        type="text" 
-                        placeholder="Direccion Residencia" 
+                        className="mb-3 form-control rounded-pill border border-dark"
+                        type="text"
+                        placeholder="Direccion Residencia"
                         value={usuario.Direccion_residencia}
-                        onChange={(e) => setUsuario({...usuario, Direccion_residencia:e.target.value})}/>
-                    <button 
-                        type="submit" 
+                        onChange={(e) => setUsuario({ ...usuario, Direccion_Residencia: e.target.value })} />
+                    <button
+                        type="submit"
                         className="btn btn-outline-secondary rounded-pill"
                         onClick={handleClick}
-                        >Sign Up</button>
+                    >Sign Up</button>
                 </div>
             </form>
         </div>
