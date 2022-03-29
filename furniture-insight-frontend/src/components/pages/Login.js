2aa2e616809b5293e5eb30d2f1efabb63816df10
Jose Mendoza <joseamendozajimenez@hotmail.com>
2022-03-29 18:29:20 -0500

    Added getting tokens when signed up

--- furniture-insight-frontend/src/components/pages/Login.js
+++ furniture-insight-frontend/src/components/pages/Login.js
@@ -1,54 +1,82 @@
 import React from "react";
-import { useState } from "react";
-import { useNavigate,Link} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import logo from "../images/logo.png";
 
-function Login({isUserLogged}) {      
+function Login({ isUserLogged }) {
     let navigate = useNavigate();
 
-    const [user, setUser] = useState({
-        email: "",
-        password: ""
+    const [usuario, setUsuario] = useState({
+        Nombre: "",
+        Contraseña: ""
     });
+
+    const [usuarioToken, setUsuarioToken] = useState({
+        accessToken: "",
+        refreshToken: ""
+    })    
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify(usuario)
+    };
+
+    const loginUsuario = () => {
+        fetch('http://localhost:8000/login', requestOptions)
+        .then(async response => {
+            const isJson = response.headers.get('content-type')?.includes('application/json');
+            const data = isJson && await response.json();            
+            if(!response.ok){
+                alert("Nombre o Contraseña invalidos")
+                const error = (data && data.message) || response.status;
+                isUserLogged(false)
+                return Promise.reject(error)
+            }
+            else {
+                isUserLogged(true)
+                navigate("/home", {replace:true})
+            }
+            setUsuarioToken(data, console.log(usuarioToken));
+        })
+    } 
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("Data entered");
-        console.log(user);
+        loginUsuario();
+        console.log(usuario)
+        
+
     }
 
-   
-    const handleClick = () => {               
-        isUserLogged(true); 
-        navigate("/home", {replace:true});         
+    const handleClick = () => {        
+        // navigate("/home", {replace:true});         
     }
-    
-
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div className="form-login">
-                <img className="mb-3" src={logo} width="60" height="65" alt="logo" />
+                    <img className="mb-3" src={logo} width="60" height="65" alt="logo" />
                     <h4 className="h4 mb-3">Log In</h4>
                     <input
                         className="mb-3 form-control rounded-pill border border-dark"
-                        type="email"
-                        placeholder="Email"
-                        value={user.email}
-                        onChange={(e) => setUser({ ...user, email: e.target.value })} />
+                        type="text"
+                        placeholder="Nombre"
+                        value={usuario.Nombre}
+                        onChange={(e) => setUsuario({ ...usuario, Nombre: e.target.value })} />
                     <input
                         className="mb-3 form-control rounded-pill border border-dark"
                         type="password"
-                        placeholder="Password"
-                        value={user.password}
-                        onChange={(e) => setUser({ ...user, password: e.target.value })} />
+                        placeholder="Contraseña"
+                        value={usuario.Contraseña}
+                        onChange={(e) => setUsuario({ ...usuario, Contraseña: e.target.value })} />
                     <div className="mb-3">
                         <Link to="/recoverpass">Forgot Password?</Link>
                     </div>
                     <button
                         type="submit"
                         className="btn btn-outline-secondary rounded-pill"
-                        onClick={handleClick}>Log In</button>
+                       >Log In</button>
                 </div>
             </form>
         </div>
