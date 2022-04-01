e414e5c9de2d800cd690f31521f8669be6e5c9b4
Jose Mendoza <joseamendozajimenez@hotmail.com>
2022-04-01 17:41:48 -0500

    Store shows Descripcion and more details about Mueble when clicked

--- furniture-insight-frontend/src/components/pages/Store.js
+++ furniture-insight-frontend/src/components/pages/Store.js
@@ -11,24 +11,22 @@ function Store() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [muebles, setMuebles] = useState([]);
+    const[muebleUpdated, setMuebleUpdated] = useState("");
 
     useEffect(() => {
         fetch('http://localhost:8000/mueble/all')
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true);
-                setMuebles(result)
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-        )
-    }, [])
-
-    console.log(muebles)
-
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setMuebles(result)
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])    
     const [busqueda, setBusqueda] = useState(
         {
             buscar: "",
@@ -43,11 +41,11 @@ function Store() {
         console.log(busqueda);
     }
 
-    if(error) {
+    if (error) {
         return <div>Error: {error.message}</div>
     } else if (!isLoaded) {
-        return <div>Loading...</div>
-    } else{
+        return <div>Cargando productos...</div>
+    } else {
         return (
             <div className="container">
                 <div className="row mt-3">
@@ -58,52 +56,93 @@ function Store() {
                 <form onSubmit={handleSubmit}>
                     <div className="row mt-3 mb-3">
                         <div className="col-3 search-bar">
-                            <input 
+                            <input
                                 className="form-control rounded-pill border border-dark"
-                                type = "text"                    
+                                type="text"
                                 placeholder="Buscar Mueble"
                                 value={busqueda.buscar}
-                                onChange={(e) => setBusqueda({...busqueda, buscar: e.target.value})} />
+                                onChange={(e) => setBusqueda({ ...busqueda, buscar: e.target.value })} />
                         </div>
                         <div className="col-1">
-                            <input 
+                            <input
                                 className="form-control rounded-pill border border-dark"
-                                type="text" 
+                                type="text"
                                 placeholder="Ancho"
                                 value={busqueda.ancho}
-                                onChange={(e) => setBusqueda({...busqueda, ancho: e.target.value})}/>
+                                onChange={(e) => setBusqueda({ ...busqueda, ancho: e.target.value })} />
                         </div>
                         <div className="col-1">
-                            <input 
-                                className="form-control rounded-pill border border-dark" 
+                            <input
+                                className="form-control rounded-pill border border-dark"
                                 type="text"
                                 placeholder="Largo"
                                 value={busqueda.largo}
-                                onChange={(e) => setBusqueda({...busqueda, largo: e.target.value})}/>
+                                onChange={(e) => setBusqueda({ ...busqueda, largo: e.target.value })} />
                         </div>
                         <div className="col">
-                            <button 
-                                type="button" 
+                            <button
+                                type="button"
                                 className="btn btn-secondary rounded-pill"
-                                onClick={handleSubmit}>                            
+                                onClick={handleSubmit}>
                                 <FontAwesomeIcon icon={faMagnifyingGlass}
                                 /> Search</button>
                         </div>
                     </div>
                 </form>
-                <div>
-                    <h2 className="h2 text-center">Muebles</h2>
-                    <div className="item-container mt-3">
-                        {muebles.map((mueble) => (
-                            <div className="cards" key={mueble.Id_Mueble}>
-                                <h3 className="h3">{mueble.Nombre}</h3>
-                                <p>{mueble.Precio}</p>
+                <div className="row">
+                    {muebles.map((mueble) => (
+                        <div className="col-auto" key={mueble.Id_Mueble} style={{ "maxWidth": "18rem" }}>
+                            <div className="card text-center">
+                                <div className="card-body">
+                                    <h5 className="card-title">{mueble.Nombre}</h5>
+                                    <p className="card-text">Precio: {mueble.Precio}</p>
+                                    <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#muebleModal" onClick={() => {setMuebleUpdated(mueble)}}>
+                                        Ver Detalles
+                                    </button>
+                                </div>
+                                <div className="modal fade" id="muebleModal" tabIndex="-1" aria-labelledby="muebleModalLabel" aria-hidden="true">
+                                    <div className="modal-dialog">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className="modal-title" id="muebleModalLabel">{muebleUpdated.Nombre}</h5>
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div className="modal-body">
+                                                <dl className="row">
+                                                    <dt className="col-sm-3">Descripcion</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Descripcion}</dd>
+
+                                                    <dt className="col-sm-3">Anchura</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Anchura}</dd>
+
+                                                    <dt className="col-sm-3">Altura</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Altura}</dd>
+
+                                                    <dt className="col-sm-3">Profundidad</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Profundidad}</dd>
+
+                                                    <dt className="col-sm-3">Medidas Adicionales</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Medidas_Adicionales}</dd>
+
+                                                    <dt className="col-sm-3">Color</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Color}</dd>
+
+                                                    <dt className="col-sm-3">Precio</dt>
+                                                    <dd className="col-sm-9">{muebleUpdated.Precio}</dd>
+                                                </dl>
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-secondary">Agregar al Carrito</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        ))}                        
-                    </div>
-                </div>                           
+                        </div>
+                    ))}
+                </div>
             </div>
-        )   
+        )
     }
 }
 
