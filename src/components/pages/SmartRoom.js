3e67c89884ea125fa2329e6d0b107fc4243a02c2
Leand3k <theleand3000@gmail.com>
2022-04-17 18:55:13 -0400

    Added canvas.

--- src/components/pages/SmartRoom.js
+++ src/components/pages/SmartRoom.js
@@ -1,36 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import bed from "../images/bed.jpg";
 import chairs from "../images/chairs.jpg";
 import table from "../images/table.jpg";
 import desk from "../images/desk.jpg";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-function SmartRoom() {
-    return (
-        <div className="container">
-            <div className="row mt-3">
-                <div className="col mb-3">
-                    <h4 className="h4 text-center"> Based on your design </h4>
-                    <p className="h6 text-center"> Recommendations</p>
-                    <img className="center img-thumbnail" src={table} alt="table.jpg"/>
-                    <img className="center img-thumbnail" src={chairs} alt="chairs.jpg"/>
-                    <img className="center img-thumbnail" src={desk} alt="desk.jpg"/>
-                </div>
-                <div className="col mb-3">
-                    <img className="center img-thumbnail" src={bed} alt="bed.jpg"/>
-                </div>
-                <div className="col mb-3">
-                    <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-toggle> Search Recommendations </button>
-                    <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-toggle> Export Grid to PDF </button>
-                </div>
-            </div>
-        </div>
-    );
+
+function SmartRoom(){
+
 }
+
+
 
 export default SmartRoom;
