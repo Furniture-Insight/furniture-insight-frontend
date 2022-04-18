5d063ef5cfc08766e6a62219ae362b1d43f13e63
Sebastian Soto <sebastiansoto.fms@gmail.com>
2022-04-18 16:18:36 -0400

    Navbar, home and profile changes

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
+import { Link } from "react-router-dom";
+import Cookies from 'universal-cookie';
 
-function Profile() {
+function Profile(isUserLogged) {
+
+    const cookies = new Cookies();   
+    const handleClick = () =>{
+        cookies.remove('Id_Usuario');
+        cookies.remove('Nombre');
+        cookies.set('Session', false, {path:'/'});
+        isUserLogged(false);        
+    }
+
+    const usuarioNombre = cookies.get('Nombre');
 
     return(
         <>
-            <h1 class="welcome-message">Welcome Juan</h1>
-            <div class="row card-deck">
-                <div class="col card border-dark m-5">
+            <h1 class="welcome-message">¡Bienvenido a tu perfil, {usuarioNombre}!</h1>
+            <div class="row card-deck justify-content-around">
+                <div class="col-3 card border-dark m-5">
                     <div class="card-body">
                         <h5 class="card-title pb-3">Account Details</h5>
                         <p class="card-text">Lorem ipsum dolor sit amet</p>
@@ -14,23 +27,19 @@ function Profile() {
                         <p class="card-text">Lorem ipsum dolor sit amet</p>
                     </div>
                 </div>
-                <div class="col card border-dark m-5">
+                <div class="col-3 card border-dark m-5">
                     <div class="card-body">
-                        <h5 class="card-title pb-3">Wish List</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
+                        <h5 class="card-title pb-3">Tu Lista</h5>
+                        <p class="card-text"> COMING SOON </p>
                     </div>
                 </div>
-                <div class="col card border-dark m-5">
+                <div class="col-3 card border-dark m-5">
                     <div class="card-body">
-                        <h5 class="card-title pb-3">Settings</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
-                        <p class="card-text">Lorem ipsum dolor sit amet</p>
+                        <h5 class="card-title pb-3">Ajustes</h5>
+                        <div class="d-grid col-5">
+                            <button type="button" class="btn btn-outline-success m-3"> Editar Datos </button>
+                            <Link type="button" class="btn btn-outline-danger m-3" to="/" onClick={handleClick}>Logout</Link>
+                        </div>
                     </div>
                 </div>
             </div>
