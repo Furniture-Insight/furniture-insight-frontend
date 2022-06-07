7b142e9716361c896f45b54f30533d3ae16bca7d
Sebastian Soto <sebastiansoto.fms@gmail.com>
2022-06-07 12:39:35 -0400

    Admin Dashboard UI

--- src/components/pages/AdminDashboard.js
+++ src/components/pages/AdminDashboard.js
@@ -1,14 +1,69 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCouch, faUser, faBed, faChair, faUsers, faHammer } from "@fortawesome/free-solid-svg-icons";
 
 function AdminDashboard(){
     return(
-        <div>
-            <button>Mueble</button>
-            <button>Categoria</button>
-            <button>Material</button>
-            <button>SubCategoria</button>
-            <button>Usuario</button>
-            <button>Admin</button>
+        <div class="box">
+            <h1 className='text-center m-5'> ¡Bienvenido al Dashboard de Administrador! </h1>
+            <h3 className='text-center m-3'> Elija un opcion: </h3>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="admin-option col-lg-3 col-md-3 col-sm-3 col-xs-12 m-3">
+                        <div class="box-part text-center">
+                            <i class="fa-3x" aria-hidden="true"><FontAwesomeIcon icon={faCouch}/></i>
+                            <div class="title">
+                                <h4>Muebles</h4>
+                            </div>
+                        </div>
+                    </div>	 
+
+                    <div class="admin-option col-lg-3 col-md-3 col-sm-3 col-xs-12 m-3">
+                        <div class="box-part text-center">
+                            <i class="fa-3x" aria-hidden="true"><FontAwesomeIcon icon={faBed}/></i>
+                            <div class="title">
+                                <h4>Categorias</h4>
+                            </div>
+                        </div>
+                    </div>	 
+                        
+                    <div class="admin-option col-lg-3 col-md-3 col-sm-3 col-xs-12 m-3">
+                        <div class="box-part text-center">
+                            <i class="fa-3x" aria-hidden="true"><FontAwesomeIcon icon={faChair}/></i>
+                            <div class="title">
+                                <h4>Subcategorias</h4>
+                            </div>
+                        </div>
+                    </div>	 
+                        
+                    <div class="admin-option col-lg-3 col-md-3 col-sm-3 col-xs-12 m-3">
+                        <div class="box-part text-center">
+                            <i class="fa-3x" aria-hidden="true"><FontAwesomeIcon icon={faHammer}/></i>
+                            <div class="title">
+                                <h4>Materiales</h4>
+                            </div>
+                        </div>
+                    </div>	 
+                        
+                    <div class="admin-option col-lg-3 col-md-3 col-sm-3 col-xs-12 m-3">
+                        <div class="box-part text-center">
+                            <i class="fa-3x" aria-hidden="true"><FontAwesomeIcon icon={faUsers}/></i>
+                            <div class="title">
+                                <h4>Usuarios</h4>
+                            </div>
+                        </div>
+                    </div>	 
+                        
+                    <div class="admin-option col-lg-3 col-md-3 col-sm-3 col-xs-12 m-3">
+                        <div class="box-part text-center">
+                            <i class="fa-3x" aria-hidden="true"><FontAwesomeIcon icon={faUser}/></i>
+                            <div class="title">
+                                <h4>Admin</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>		
+            </div>
         </div>
     )
 }
